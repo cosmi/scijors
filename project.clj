0a4bb76c4076003e6842c12fbed27c86f631ef1c
Marcin Skotniczny <cosmi.cosmi@gmail.com>
2013-12-10 10:30:59 +0100

    Bump 0.1.0-RC

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject scijors "0.1.0"
+(defproject org.cosmi.scijors "0.1.0-RC"
   :description "An easy to use, ultra-high performance, HTML-based Clojure templating library."
   :url "https://github.com/cosmi/scijors"
   :license {:name "Eclipse Public License"
