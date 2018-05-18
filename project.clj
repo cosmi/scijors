5c72dbaabc589b53b216b507952782b5c6d909b0
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2018-05-18 17:42:13 +0200

    Update version of instaparse and Clojure

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject scijors "0.1.4"
+(defproject scijors "0.1.5"
   :description "An easy to use, ultra-high performance, HTML-based Clojure templating library."
   :url "https://github.com/cosmi/scijors"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [instaparse "1.2.7"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [instaparse "1.4.9"]])
 
 
